e8aaccdd1a469136fd4297b674d110239b434df6
alwinruby <alwinsolanky@gmail.com>
2021-08-27 04:52:04 +0100

    first program

---
+++ src/java_programming_concepts_1/Aprogram.java
@@ -0,0 +1,10 @@
+package java_programming_concepts_1;
+
+public class Aprogram {
+    public static void main(String args []){
+        System.out.println("Yo!");
+        System.out.print("Are you ");
+        System.out.print("ready?\n");
+        System.out.println("A success");
+    }
+}
