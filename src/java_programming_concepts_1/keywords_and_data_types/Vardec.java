c7bf8cb2fa67679135ff8b549621297b175c7353
alwinruby <alwinsolanky@gmail.com>
2021-09-03 12:39:53 +0100

    Vardecs

---
+++ src/java_programming_concepts_1/keywords_and_data_types/Vardec.java
@@ -0,0 +1,23 @@
+package java_programming_concepts_1.keywords_and_data_types;
+
+public class Vardec {
+
+    static int i;
+    static byte b;
+    static float f;
+    static char c;
+    static short s;
+    static double d;
+    static boolean bool;
+
+    public static void main(String[] args){
+        System.out.println("Variable Declarations \n");
+        System.out.println("Value of int " + i);
+        System.out.println("Value of byte " + b);
+        System.out.println("Value of float " + f);
+        System.out.println("Value of char " + c);
+        System.out.println("Value of short " + s);
+        System.out.println("Value of double " + d);
+        System.out.println("Value of boolean " + bool);
+    }
+}
---
+++ src/java_programming_concepts_1/keywords_and_data_types/Vardec2.java
@@ -0,0 +1,23 @@
+package java_programming_concepts_1.keywords_and_data_types;
+
+public class Vardec2 {
+
+//    public static void main(String[] args){
+//        int i;
+//        byte b;
+//        float f;
+//        char c;
+//        short s;
+//        double d;
+//        boolean bool;
+//        System.out.println("Variable Declarations \n");
+//        System.out.println("Value of int " + i);
+//        System.out.println("Value of byte " + b);
+//        System.out.println("Value of float " + f);
+//        System.out.println("Value of char " + c);
+//        System.out.println("Value of short " + s);
+//        System.out.println("Value of double " + d);
+//        System.out.println("Value of boolean " + bool);
+//    }
+
+}
